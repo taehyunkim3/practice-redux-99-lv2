a6a84dd0579061e5aea1b0f9ec08030da345e3e8
Taehyun Kim <agart319@gmail.com>
2023-06-27 11:37:33 +0900

    vanilla JS WITHOUT Redux
    vanilla JS WITHOUT Redux

---
+++ vanilla-redux/src/index.js
@@ -0,0 +1,31 @@
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+
+let count = 0;
+
+number.innerText = count;  // 한번만 일어남 .
+
+const updateText = () => {
+  number.innerText = count;
+}
+
+
+const handleAdd = () => {
+  count++;
+  updateText();
+}
+
+
+const handleMinus = () => {
+  count--;
+  updateText();
+}
+
+
+
+
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
