5be123ace3c4ffc085e0e00812ae9d3f2a9da4ce
Taehyun Kim <agart319@gmail.com>
2023-06-27 15:19:52 +0900

    vanillaTODO redux넣는중

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,17 +1,18 @@
 import { createStore } from "redux";
-
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO"
+//NEVER MUTATE STATE
 
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+
+      return [...state, { text: action.text, id: Date.now() }];         //ES6 spread, push를 새 array로 반환하는 방법으로 바꾼것.
     case DELETE_TODO:
       return [];
     default:
@@ -21,6 +22,9 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()))      //자료저장소 추가
+
+
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
@@ -29,3 +33,9 @@ const onSubmit = e => {
 };
 
 form.addEventListener("submit", onSubmit);
+
+
+
+//single source of truth
+//state를 변경할땐 action을 사용해야하만 함.
+//state를 직접 변경하지 않고 new state를 반환해야만 함.
