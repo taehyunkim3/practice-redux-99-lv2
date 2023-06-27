16e5bfeea161fc541916dc45212c12205455330a
Taehyun Kim <agart319@gmail.com>
2023-06-27 14:36:02 +0900

    VANILLA TODO start
    VANILLA TODO start

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -2,45 +2,34 @@ import { createStore } from "redux";
 
 
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "add";  // 자바스크립트가 오타 감지해줌!
-const MINUS = "minus";
-
-const countModifier = (count = 0, action) => {  //기본값 지정, action은 reducer에게 말하는것.
+const reducer = (state = [], action) => {
+  console.log(action);
   switch (action.type) {
-    case ADD:             //switch는 string만 가능함.
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
 
 
 }  // reducer 는 데이터를 변경시키는 함수function
 
+const store = createStore(reducer);
 
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
+};
 
-const countStore = createStore(countModifier);
-// console.log(countStore)
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-}
-
-
-countStore.subscribe(onChange);
-
-// countStore.dispatch({ type: 'add' })
-
-
-const handleMinus = () => {
-  countStore.dispatch({ type: 'minus' });
-}
-
-add.addEventListener("click", () => { countStore.dispatch({ type: 'add' }) });
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
