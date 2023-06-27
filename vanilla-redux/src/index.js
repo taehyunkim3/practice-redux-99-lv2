734659795664b7764b321af83c7be01f18eb0460
Taehyun Kim <agart319@gmail.com>
2023-06-27 18:15:10 +0900

    ReactReduxSetting

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import App from './components/App.js';
+// 원래부터 있던 코드
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
+// 우리가 추가할 코드
+import store from "./components/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 
-  <App />
-
+  //App을 Provider로 감싸주고, configStore에서 export default 한 store를 넣어줍니다.
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
-
 
---
+++ vanilla-redux/src/modules/Counter.js
@@ -0,0 +1,12 @@
+const initialState = {
+    number: 0,
+};
+
+const Counter = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default Counter
