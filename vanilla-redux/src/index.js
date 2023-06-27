62a223878db0b496030dd383f69355d1de37dc12
Taehyun Kim <agart319@gmail.com>
2023-06-27 13:57:56 +0900

    vanilla Redux 마지막

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -6,15 +6,20 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0, action) => {  //기본값 지정, action은 reducer에게 말하는것.
 
-  if (action.type === 'add') {
-    return count + 1;   // count++ 하면 안됨!!!주의!!
-  } else if (action.type === 'minus') {
-    return count - 1;
-  } else {
-    return count;
+const ADD = "add";  // 자바스크립트가 오타 감지해줌!
+const MINUS = "minus";
+
+const countModifier = (count = 0, action) => {  //기본값 지정, action은 reducer에게 말하는것.
+  switch (action.type) {
+    case ADD:             //switch는 string만 가능함.
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
+
 
 }  // reducer 는 데이터를 변경시키는 함수function
 
