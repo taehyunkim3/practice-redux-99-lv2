357201c541cf585b1fe7ae542aeb8b3120e4b7e3
Taehyun Kim <agart319@gmail.com>
2023-06-27 12:00:49 +0900

    Update index.js

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -6,9 +6,10 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  console.log(state)
-  return state;
+const countModifier = (count = 0, action) => {  //기본값 지정, action은 reducer에게 말하는것.
+
+  console.log(count, action)
+  return count;
 }  // reducer 는 데이터를 변경시키는 함수function
 
 const countStore = createStore(countModifier);
