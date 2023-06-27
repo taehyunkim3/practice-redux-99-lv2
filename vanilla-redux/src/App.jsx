72d5902b06eece6e9f2390020e5a21d782cf2cf4
Taehyun Kim <agart319@gmail.com>
2023-06-28 07:52:40 +0900

    payload넣는중

--- vanilla-redux/src/App.jsx
+++ vanilla-redux/src/App.jsx
@@ -1,8 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
-
+import { PLUS_ONE, MINUS_ONE, plusOne} from "./redux/modules/Counter";
+import { useEffect, useState } from "react";
 
 
 function App() {
+
+    const [number, setNumber] = useState(0);
 
 
 //여기에서 store에 접근하여 counter의 값을 읽어오고 싶다.
@@ -12,22 +15,31 @@ const counter = useSelector((state)=>{
 return state.counter
 });
 
+useEffect(()=>{
+    console.log("number", number)
+}, [number])
+
+
+
+
 const dispatch = useDispatch();
 
-console.log("counter->", counter.number)
+// console.log("counter->", counter.number)
 
     return (
         <>
                 <div> 현재 카운트:{counter.number}</div>
+                <input type="number" value={number} onChange={(e)=>{
+                    setNumber(e.target.value)
+                }}></input>
                 <button onClick={()=>{
-                         dispatch({
-                            type:"PLUS_ONE"
-                         })
+                         dispatch(plusOne())
                          //+1로직 넣어주기
                 }}>+</button>
+                {/* 아래 형태를 위에처럼 변경함(일부러 놔둠) */}
                              <button onClick={()=>{
                          dispatch({
-                            type:"MINUS_ONE"
+                            type:MINUS_ONE
                          })
                          //+1로직 넣어주기
                 }}>-</button>
--- vanilla-redux/src/redux/modules/Counter.js
+++ vanilla-redux/src/redux/modules/Counter.js
@@ -1,3 +1,21 @@
+//action value,
+export const PLUS_ONE = "PLUS_ONE";
+export const MINUS_ONE = "MINUS_ONE";
+
+//action creator : action value를 reutrn하는 함수  app.js
+export const plusOne = () => {
+    return {
+        type: PLUS_ONE,
+    }
+}
+export const minusOne = () => {
+    return {
+        type: MINUS_ONE,
+    }
+}
+
+
+
 //초기상태값
 const initualState = {
     number: 0
@@ -9,16 +27,17 @@ const initualState = {
 //(1) state를 action의 type에 따라 변경하는 함수
 
 //input에선 state, action 두개 받음요!
-////action에는 type, value
+//action객체라는 것은 action type을 payload만큼 처리
+
 
 
 const counter = (state = initualState, action) => {
     switch (action.type) {
-        case "PLUS_ONE":
+        case PLUS_ONE:
             return {
                 number: state.number + 1
             }
-        case "MINUS_ONE":
+        case MINUS_ONE:
             return {
                 number: state.number - 1
             }
