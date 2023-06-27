9f7650177c866ccf899c286c1abd732c96e93eaa
Taehyun Kim <agart319@gmail.com>
2023-06-27 14:49:41 +0900

    VANILLATODO start source

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,35 +1,19 @@
-import { createStore } from "redux";
-
-
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const reducer = (state = [], action) => {
-  console.log(action);
-  switch (action.type) {
-    case ADD_TODO:
-      return [];
-    case DELETE_TODO:
-      return [];
-    default:
-      return state;
-  }
-
-
-}  // reducer 는 데이터를 변경시키는 함수function
-
-const store = createStore(reducer);
+const createToDo = toDo => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
+};
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  createToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
