2b52d168f273a4a6ab5dcbeb5587101cd09e5e83
Taehyun Kim <agart319@gmail.com>
2023-06-28 10:59:39 +0900

    layout, children

--- vanilla-redux/src/pages/Works.jsx
+++ vanilla-redux/src/pages/Works.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
 
 const Works = () => {
 
     const navigate = useNavigate();
+    const location = useLocation();
+    const {param} = useParams();
+    console.log(param)
+    console.log(location)
   return (<>
   <div>Works</div>
   <button onClick={()=>{navigate("/")}}>home으로 돌아가기</button>
+  <Link to='/contact'>컨텍트페이지로이동</Link>
   
   </>
   )
--- vanilla-redux/src/shared/Router.js
+++ vanilla-redux/src/shared/Router.js
@@ -7,19 +7,21 @@ import About from "../pages/About";
 import Works from "../pages/Works";
 import Contact from "../pages/Contact";
 import Page404 from "../pages/Page404";
+import Layout from "./layout";
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="about" element={<About />} />
-                <Route path="works" element={<Works />} />
-                <Route path="contact" element={<Contact />} />
-                <Route path="*" element={<Page404 />} />
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="works" element={<Works />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<Page404 />} />
 
-            </Routes>
-
+                </Routes>
+            </Layout>
         </BrowserRouter>
     )
 }
